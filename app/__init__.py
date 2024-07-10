d95a47dba76bebc39475610ef4dfd2ea8304d9a9
ashunair <ashwathyair1998@gmail.com>
2024-07-10 15:23:21 -0400

    application code changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
-# app/__init__.py
 from flask import Flask
+import os
 
 
 def create_app():
     app = Flask(__name__)
+
+    # Define the template folder path
+    template_folder = os.path.join(app.root_path, 'templates')
+
+    # Debugging information
+    print("Template folder path:", template_folder)
+
+    # Check if the directory exists and list its contents
+    if os.path.exists(template_folder):
+        print("Templates available:", os.listdir(template_folder))
+    else:
+        print("Template folder does not exist")
 
     from .routes import main
     app.register_blueprint(main)
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
-# app/routes.py
 from flask import Blueprint, render_template, request, redirect
 from datetime import datetime
 
 main = Blueprint('main', __name__)
 messages = []
 
+
 @main.route('/')
 def index():
     return render_template('index.html', messages=messages)
+
 
 @main.route('/send', methods=['POST'])
 def send():
